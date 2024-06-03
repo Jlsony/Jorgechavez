60bc22cc6a207f4e673233d064f4d2e99df822b5
Jlsony <166874583+Jlsony@users.noreply.github.com>
2024-06-03 14:43:34 +0800

    Reto3
    Reto3

---
+++ Reto3/Reto3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+Console.WriteLine("ACADEMIA.NET");
+Console.WriteLine("Autor: Jorge Chavez");
+
+ int[] arr = { 20, 10, 8, 3, 5, -1, 2, -4, -8,};
+        Console.WriteLine($"El número mínimo es {arr.Min()}");
+        Console.WriteLine($"El número máximo es {arr.Max()}");
